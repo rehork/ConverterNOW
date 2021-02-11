1fce9dd1fe7db2bc680b9249d96364a13b573ad5
Damiano <damianoferrari1998@gmail.com>
2021-02-11 16:55:58 +0100

    manual update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,44 +3,45 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "529f9281c7bd448463c995b47c9befe4",
-"icons/Icon-192.png": "fcfd9515e216757d0c405b04a0741a21",
-"manifest.json": "33ad2b4a1951c70756e32f9796b94618",
-"assets/resources/lang/en.json": "0538b0c12c9b4672de9c672c25dc4b74",
-"assets/resources/lang/nb.json": "81f11f401fac554a3c0261c0fb36eb32",
-"assets/resources/lang/it.json": "7c046a3bbe5d2d7825efc846d8a85752",
-"assets/resources/lang/pt.json": "8c4a8255bf622d0b071e1a38b7709216",
-"assets/resources/lang/fr.json": "4192b9183c71de45c18f176a47c94b07",
-"assets/resources/images/angoli.png": "f8a0c60156c1d7a67f6781422b785f8c",
-"assets/resources/images/prefissi.png": "69a1b0eed46990d3ff841d2c328cf94a",
-"assets/resources/images/consumo.png": "d557b7b8da7a9887bb5dfa73c83c9e61",
-"assets/resources/images/potenza.png": "92138ef7faa7e8f6fd136c265d7c4932",
-"assets/resources/images/volume.png": "4241670b1d26ac8ca9dfd6a24e7b4b86",
-"assets/resources/images/calculator.png": "5080b8b64bfb548edabd5f354018f2a0",
-"assets/resources/images/valuta.png": "99a5c05753997b4ce9ce8097686c38ca",
-"assets/resources/images/logo.png": "59b984028582182089ccdfbbc46f10c7",
-"assets/resources/images/pressione.png": "a98980f0401565315a9e9bfb48627ffa",
-"assets/resources/images/massa.png": "563a37b10d72222dee86a42b251d9441",
-"assets/resources/images/energia.png": "103ed054f04d796a0c91d169e07fca80",
-"assets/resources/images/tempo.png": "62219900f6e903a5ca20710b1a9b6aa1",
-"assets/resources/images/temperatura.png": "1ec875c848337b5a1e5196e78ff9cd7f",
-"assets/resources/images/scarpe.png": "d189f8dac179d2fc68ab257f5192d537",
-"assets/resources/images/area.png": "fa724188ecbd91ecdbde6c0478ce26e3",
-"assets/resources/images/forza.png": "63a1463fe0d29ca96122e74bf1e45605",
-"assets/resources/images/dati.png": "cb5d3bfa5d85090037d249f234596f87",
-"assets/resources/images/conversione_base.png": "b82321121be9324d77b9a9a39b4c5228",
-"assets/resources/images/torque.png": "48c7d3cb1c1c2866b166de7d8e995075",
-"assets/resources/images/lunghezza.png": "2d57f006df44e79fa5a3b243a63bfcf1",
-"assets/resources/images/velocita.png": "a5dbe972024e38c2a26dd9854eb22438",
-"assets/NOTICES": "c5c2054aed2b584c7668be0efecf32be",
-"assets/AssetManifest.json": "7224c4deede04bfeec60fe47f7697d11",
+  "assets/AssetManifest.json": "a31c2cdf0c96fc2e22de5c5fdfdf8e69",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"index.html": "c7cd4aece94805b823619d156abbd039",
-"/": "c7cd4aece94805b823619d156abbd039",
-"version.json": "8bbbcfe91c48c7aa0307ec8df82600c2",
-"main.dart.js": "18d164aaee2a48875c4a7c2892bfea10",
-"favicon.png": "9e118249f3d09c38b28095dbb5c14a7e"
+"assets/NOTICES": "63de55c9e299851389cde2853211b245",
+"assets/resources/images/angoli.png": "f8a0c60156c1d7a67f6781422b785f8c",
+"assets/resources/images/area.png": "fa724188ecbd91ecdbde6c0478ce26e3",
+"assets/resources/images/calculator.png": "5080b8b64bfb548edabd5f354018f2a0",
+"assets/resources/images/consumo.png": "d557b7b8da7a9887bb5dfa73c83c9e61",
+"assets/resources/images/conversione_base.png": "b82321121be9324d77b9a9a39b4c5228",
+"assets/resources/images/dati.png": "cb5d3bfa5d85090037d249f234596f87",
+"assets/resources/images/energia.png": "103ed054f04d796a0c91d169e07fca80",
+"assets/resources/images/forza.png": "63a1463fe0d29ca96122e74bf1e45605",
+"assets/resources/images/logo.png": "59b984028582182089ccdfbbc46f10c7",
+"assets/resources/images/lunghezza.png": "2d57f006df44e79fa5a3b243a63bfcf1",
+"assets/resources/images/massa.png": "563a37b10d72222dee86a42b251d9441",
+"assets/resources/images/potenza.png": "92138ef7faa7e8f6fd136c265d7c4932",
+"assets/resources/images/prefissi.png": "69a1b0eed46990d3ff841d2c328cf94a",
+"assets/resources/images/pressione.png": "a98980f0401565315a9e9bfb48627ffa",
+"assets/resources/images/scarpe.png": "d189f8dac179d2fc68ab257f5192d537",
+"assets/resources/images/temperatura.png": "1ec875c848337b5a1e5196e78ff9cd7f",
+"assets/resources/images/tempo.png": "62219900f6e903a5ca20710b1a9b6aa1",
+"assets/resources/images/torque.png": "48c7d3cb1c1c2866b166de7d8e995075",
+"assets/resources/images/valuta.png": "99a5c05753997b4ce9ce8097686c38ca",
+"assets/resources/images/velocita.png": "a5dbe972024e38c2a26dd9854eb22438",
+"assets/resources/images/volume.png": "4241670b1d26ac8ca9dfd6a24e7b4b86",
+"assets/resources/lang/de.json": "70ef19c37d524d0fde4263bff5f42665",
+"assets/resources/lang/en.json": "e4537517c2d725e5178e424a6273d883",
+"assets/resources/lang/fr.json": "16ca1890e100d096f0ce65d990ab9ecb",
+"assets/resources/lang/it.json": "8594c83c2cc296129aef5e494a9d430d",
+"assets/resources/lang/nb.json": "df64c5cf1b12f3fc482963842e07edcb",
+"assets/resources/lang/pt.json": "d5d44c6e072d0649d45d24912040f322",
+"favicon.png": "9e118249f3d09c38b28095dbb5c14a7e",
+"icons/Icon-192.png": "fcfd9515e216757d0c405b04a0741a21",
+"icons/Icon-512.png": "529f9281c7bd448463c995b47c9befe4",
+"index.html": "3202ea9a87a78666b6b620194a2bda3e",
+"/": "3202ea9a87a78666b6b620194a2bda3e",
+"main.dart.js": "6f2698987eb878f8110e3d5656911717",
+"manifest.json": "7de0385e1fb63cb5e95432f5b91e3a96",
+"version.json": "8bbbcfe91c48c7aa0307ec8df82600c2"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -184,7 +185,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
