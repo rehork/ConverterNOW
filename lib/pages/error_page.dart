ca558483c6409520721cbf120cba97e63b1a9844
Damiano Ferrari <damianoferrari1998@gmail.com>
2022-08-07 14:49:50 +0200

    Moved internationalization to separate package

--- lib/pages/error_page.dart
+++ lib/pages/error_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:l10n/l10n.dart';
 
 class ErrorPage extends StatelessWidget {
   const ErrorPage({Key? key}) : super(key: key);
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,7 +1,7 @@
 import 'package:converterpro/utils/property_unit_list.dart';
 import 'package:converterpro/utils/utils_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:l10n/l10n.dart';
 
 class CustomSearchDelegate extends SearchDelegate<int> {
   final List<int> orderList;
